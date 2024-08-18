8c6dc575c43515db99aab8cdab93ada1206b9457
StephenEkwedike <stephen.toch@gmail.com>
2024-08-17 18:57:41 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import printMe from './print.js';
 import './style.css'
 import Icon from './icon.png';
 
-const btn = document.createElement('div')
-btn.setAttribute('class','redcolor')
+const btn = document.createElement('button')
+btn.classList.add('redcolor');
+// btn.setAttribute('class','redcolor')
 
 document.body.appendChild(btn)
